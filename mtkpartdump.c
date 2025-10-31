04ed94371eeb889fc07ac070a6fda1d1ddd324c2
jsoltan226 <johaness.soltan@gmail.com>
2025-11-01 00:13:26 +0100

    Added more useful info in the dump

--- mtkpartdump.c
+++ mtkpartdump.c
@@ -11,7 +11,12 @@
 static i32 setup_log(void);
 
 static void dump_part_header(struct mtk_partition_header_data *hdr);
+static void dump_ext_part_header(struct mtk_part_header_extension *ext);
 
+static u32 get_aligned_part_size(struct mtk_partition_header_data *hdr);
+static u64 get_full_part_size(struct mtk_partition_header_data *hdr);
+static u64 get_full_aligned_part_size(struct mtk_partition_header_data *hdr);
+static void * get_full_memory_address(struct mtk_partition_header_data *hdr);
 static const char *get_img_type_string(u32 img_type);
 
 int main(int argc, char **argv)
@@ -21,7 +26,7 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    s_log_debug("mtkpartdump");
+    s_log_debug(MODULE_NAME);
 
     if (argc <= 1 || !strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
         s_log_error("Not enough arguments");
@@ -122,23 +127,19 @@ static void dump_part_header(struct mtk_partition_header_data *hdr)
     s_log_info("union mtk_partition_header hdr = {");
     s_log_info("    .data = {");
     log_magic ("        .magic = ", hdr->magic);
-    s_log_info("        .part_size = %#x,", hdr->part_size);
+    s_log_info("        .part_size = %#x, "
+        "// aligned: %#x, full: %#x, aligned full: %#x",
+        hdr->part_size,
+        get_aligned_part_size(hdr),
+        get_full_part_size(hdr),
+        get_full_aligned_part_size(hdr)
+    );
     s_log_info("        .part_name = \"%.32s\",", hdr->part_name);
-    s_log_info("        .memory_address = %#x,", hdr->memory_address);
+    s_log_info("        .memory_address = %p, // full: %p",
+        hdr->memory_address, get_full_memory_address(hdr));
     s_log_info("        .memory_address_mode = %#x,", hdr->memory_address_mode);
     s_log_info("        .ext = {");
-    log_magic ("            .magic = ", hdr->ext.magic);
-    s_log_info("            .hdr_size = %#x,", hdr->ext.hdr_size);
-    s_log_info("            .hdr_version = %#x,", hdr->ext.hdr_version);
-    s_log_info("            .img_type = %#x, // (%s)", hdr->ext.img_type,
-            get_img_type_string(hdr->ext.img_type));
-    s_log_info("            .is_image_list_end = %#x,",
-            hdr->ext.is_image_list_end);
-    s_log_info("            .size_alignment_bytes = %#x,",
-            hdr->ext.size_alignment_bytes);
-    s_log_info("            .part_size_hi = %#x,", hdr->ext.part_size_hi);
-    s_log_info("            .memory_address_hi = %#x",
-            hdr->ext.memory_address_hi);
+    dump_ext_part_header(&hdr->ext);
     s_log_info("        }");
     s_log_info("    }");
     s_log_info("};");
@@ -146,6 +147,77 @@ static void dump_part_header(struct mtk_partition_header_data *hdr)
 
     s_configure_log_line(S_LOG_VERBOSE, old_line_verbose, NULL);
     s_configure_log_line(S_LOG_INFO, old_line_info, NULL);
+}
+
+static void dump_ext_part_header(struct mtk_part_header_extension *ext)
+{
+    const char *old_line_verbose = NULL, *old_line_info = NULL;
+    s_configure_log_line(S_LOG_VERBOSE, "%s\n", &old_line_verbose);
+    s_configure_log_line(S_LOG_INFO, "%s\n", &old_line_info);
+
+    log_magic ("            .magic = ", ext->magic);
+    if (ext->magic != MTK_PART_EXT_MAGIC) {
+        s_log_info("            // magic mismatch (expected 0x%.8x); "
+            "skipping the rest of the extended header",
+            MTK_PART_EXT_MAGIC);
+        return;
+    }
+    s_log_info("            .hdr_size = %#x,", ext->hdr_size);
+    s_log_info("            .hdr_version = %#x,", ext->hdr_version);
+    s_log_info("            .img_type = %#x, // (%s)", ext->img_type,
+            get_img_type_string(ext->img_type));
+    s_log_info("            .is_image_list_end = %#x,",
+            ext->is_image_list_end);
+    s_log_info("            .size_alignment_bytes = %#x,",
+            ext->size_alignment_bytes);
+    s_log_info("            .part_size_hi = %#x,", ext->part_size_hi);
+    s_log_info("            .memory_address_hi = %#x",
+            ext->memory_address_hi);
+
+    s_configure_log_line(S_LOG_VERBOSE, old_line_verbose, NULL);
+    s_configure_log_line(S_LOG_INFO, old_line_info, NULL);
+}
+
+static u32 get_aligned_part_size(struct mtk_partition_header_data *hdr)
+{
+    if (hdr->ext.magic == MTK_PART_EXT_MAGIC &&
+        hdr->ext.size_alignment_bytes != 0)
+    {
+        /* Round up to the next multiple of `align` */
+        const u32 size = hdr->part_size;
+        const u32 align = hdr->ext.size_alignment_bytes;
+        return ((size + align - 1) / align) * align;
+    } else {
+        return hdr->part_size;
+    }
+}
+
+static u64 get_full_part_size(struct mtk_partition_header_data *hdr)
+{
+    if (hdr->ext.magic == MTK_PART_EXT_MAGIC) {
+        const u64 high = (u64)hdr->ext.part_size_hi << 32;
+        return high | hdr->part_size;
+    } else {
+        return (u64)hdr->part_size;
+    }
+}
+
+static u64 get_full_aligned_part_size(struct mtk_partition_header_data *hdr)
+{
+    const u32 low = get_aligned_part_size(hdr);
+    const u64 high = get_full_part_size(hdr) & 0xFFFFFFFF00000000ULL;
+
+    return high | low;
+}
+
+static void * get_full_memory_address(struct mtk_partition_header_data *hdr)
+{
+    if (hdr->ext.magic == MTK_PART_EXT_MAGIC) {
+        const u64 high = (u64)hdr->ext.memory_address_hi << 32;
+        return (void *)(high | hdr->memory_address);
+    } else {
+        return (void *)((u64)hdr->memory_address);
+    }
 }
 
 static const char *get_img_type_string(u32 img_type)
